6e065630ddcaab0f2dff60b04114335a2906f2d1
Adrian Rivera <40322466+onory@users.noreply.github.com>
2022-02-04 17:18:56 -0600

    Modificacion EndPoint create y update
    Se modifican metodos Pot para crear el ezchema de url short conciderando valore de beforeUrl y date
    Se modifica enpotint para actualizar Url larga asignada a Short, generando un historico de cambios en cada update

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //saaff url short
 
-const express = require('express')
+const express = require('express');
+const { Date } = require('mongoose');
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
-const update = require('./models/shortUrl')
+
 
 const app = express()
 app.use(express.json()); // reconoce json
@@ -26,10 +27,11 @@ app.get('/', async (req, res) => {
 //endpoint
 //peticion post de shot url para leer en index fullurl
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+  await ShortUrl.create({ UrlDestination: req.body.fullUrl })
 
   res.redirect('/')
 })
+
 
 //peticion get de shot url para mostrarse en index para shorturl
 app.get('/:shortUrl', async (req, res) => {
@@ -39,30 +41,117 @@ app.get('/:shortUrl', async (req, res) => {
   shortUrl.clicks++
   shortUrl.save()
 
-  res.redirect(shortUrl.full)
+  res.redirect(shortUrl.UrlDestination)
 })
 
 
 //peticion post actualiza newUrlFull
 app.post('/update', async (req, res) => {
 
-const idMod = req.body.idchange;
-const newUrl = req.body.newUrlFull;
+  const idMod = req.body.idchange;
+  const newUrl = req.body.newUrlFull;
 
-console.log('data contiene 1:', idMod);
-console.log('data contiene 2:', newUrl);
+  console.log('data contiene 1:', idMod);
+  console.log('data contiene 2:', newUrl);
 
-await ShortUrl.findOneAndUpdate({
-  _id : idMod
-},{
-  full: newUrl
+  await ShortUrl.findOneAndUpdate({
+    _id: idMod
+  }, {
+    UrlDestination: newUrl
+  })
+
+  res.redirect('/')
 })
 
- res.redirect('/')
+
+//----- Modificaciones de Relacion con historic -----//
+
+
+//peticion post para crear un short y un nuevo historico y base de historic sin modificar en bruto
+app.post('/shortUrlsXXX', async (req, res) => {
+  await ShortUrl.create({
+    UrlDestination: req.body.fullUrl,
+    BeforeUrl: req.body.fullUrl,
+    dateChange: new Date()
+  })
+
+  res.redirect('/')
 })
 
+//peticion post actualiza newUrlFull
+app.post('/updatexxx', async (req, res) => {
+
+  const idMod = req.body.idchange;
+  const newUrl = req.body.newUrlFull;
+  //const oldUrl = await ShortUrl.find({ "full": { _id: idMod } })
+  const oldUrl = await ShortUrl.find( { _id: idMod }, {UrlDestination:1, _id:0} );
+  const clickFull = await ShortUrl.find( { _id: idMod }, {clicks:1, _id:0} );
+  const updateDate = new Date();
+  const regiterHistoric = `urlBefore: ${oldUrl}, UrlDestination: ${newUrl}, dateUpdate:${updateDate}, UrlDestinationClicks: ${clickFull}`; //concatena informacion historic
+
+
+  //$addToSet - agrega nuevos valores, cambiando Url larga
+  await ShortUrl.findOneAndUpdate({ _id: idMod },
+
+    {
+      UrlDestination: newUrl,
+      BeforeUrl: oldUrl,
+      dateChange: updateDate
+    })
+
+    //agrega historico en bruto 
+  await ShortUrl.findOne({ _id: idMod },
+
+    {
+      $push:
+      {
+        dateChange: regiterHistoric
+      }
+    }
+
+  )
+
+  res.redirect('/')
+
+})
+
+//peticion que asigna shortUrl a Asset, se debe direccionar enpoint a asset
+app.put('/:idasset/:shortUrl'), async (req, res) => {
+
+  const { idasset } = req.param;
+  const { shortUrl } = req.param;
+
+  await Asset.findOneAndUpdate(idasset, { $push: { url: shortUrl } });
+
+  /*
+  
+  db.asset.aggregate([
+  {$lookup:
+  
+  {
+    from:'ShortUrl',
+    localFiels:'short', //crear
+    foreignField:'idShort',
+    as:'shortAsingId'
+  
+    }
+  
+  }
+    ]);
+  
+  */
+
+  res.redirect('/')
+
+}
 
 
 
 app.listen(process.env.PORT || 5000);
+
+
+
+
+
+
 
